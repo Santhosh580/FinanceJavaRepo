17a2f084596b9778e33eca6db363f59a4b74d280
Santhosh-Git <santhoshgeetha98@gmail.com>
2020-10-24 01:41:53 +0530

    First commit

---
+++ Project-glad/src/main/java/com/lti/project/Card.java
@@ -0,0 +1,69 @@
+package com.lti.project;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tbl_card")
+public class Card {
+
+	@Id
+	@SequenceGenerator(name="cardSeq",initialValue=3001,allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE,generator="cardSeq")
+	int cardNumber;
+	String cardType;
+	double maxAmount;
+	double amountAvailable;
+	
+	@OneToOne
+	@JoinColumn(name="customer_id")
+	Customer customer;
+
+	public int getCardNumber() {
+		return cardNumber;
+	}
+
+	public void setCardNumber(int cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+
+	public String getCardType() {
+		return cardType;
+	}
+
+	public void setCardType(String cardType) {
+		this.cardType = cardType;
+	}
+
+	public double getMaxAmount() {
+		return maxAmount;
+	}
+
+	public void setMaxAmount(double maxAmount) {
+		this.maxAmount = maxAmount;
+	}
+
+	public double getAmountAvailable() {
+		return amountAvailable;
+	}
+
+	public void setAmountAvailable(double amountAvailable) {
+		this.amountAvailable = amountAvailable;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	
+
+}
